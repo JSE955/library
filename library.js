af9f61c3e9f76f88ac3c779b73ee21ce099390a2
JEdwards <jordanscottedwards@gmail.com>
2023-03-18 22:52:29 -0400

    Implement delete button functionality

--- library.js
+++ library.js
@@ -21,13 +21,15 @@ const addForm = document.querySelector('.addForm');
 const submitBook = document.querySelector('.submitBook');
 addForm.style.display = 'none';
 
+// Load test data into library array
 let test1 = new Book('Batman', 'Kevin Conroy', 35, false);
 let test2 = new Book('Lion King', 'Disney', 100, true);
 addBookToLibrary(test1);
 addBookToLibrary(test2);
 
 const displayBooks = () => {
-    myLibrary.forEach((book) => {
+    document.querySelector('.bookTable').innerHTML = '';
+    myLibrary.forEach((book, index) => {
         let newRow = document.createElement('tr');
         let titleCell = document.createElement('td');
         let authorCell = document.createElement('td');
@@ -36,13 +38,19 @@ const displayBooks = () => {
         let deleteCell = document.createElement('td');
         let deleteBtn = document.createElement('button');
         deleteBtn.className = 'deleteBtn';
+        deleteBtn.setAttribute('data-index', index);
     
         titleCell.appendChild(document.createTextNode(book.title))
         authorCell.appendChild(document.createTextNode(book.author))
         pagesCell.appendChild(document.createTextNode(book.pages))
         readCell.appendChild(document.createTextNode(book.read))
         deleteBtn.appendChild(document.createTextNode('Delete'))
-        
+
+        deleteBtn.addEventListener('click', (event) => {
+            let index = event.target.dataset.index;
+            myLibrary = myLibrary.filter(book => myLibrary.indexOf(book) != index);
+            displayBooks();
+        })
     
         newRow.appendChild(titleCell);
         newRow.appendChild(authorCell);
@@ -52,39 +60,17 @@ const displayBooks = () => {
         deleteCell.appendChild(deleteBtn);
     
         bookTable.appendChild(newRow);
+        console.log(deleteBtn.dataset.index);
+
     })
 }
 
-const addBookRow = (book) => {
-    let newRow = document.createElement('tr');
-    let titleCell = document.createElement('td');
-    let authorCell = document.createElement('td');
-    let pagesCell = document.createElement('td');
-    let readCell = document.createElement('td');
-    let deleteCell = document.createElement('td');
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'deleteBtn';
-
-    titleCell.appendChild(document.createTextNode(book.title))
-    authorCell.appendChild(document.createTextNode(book.author))
-    pagesCell.appendChild(document.createTextNode(book.pages))
-    readCell.appendChild(document.createTextNode(book.read))
-    deleteBtn.appendChild(document.createTextNode('Delete'))
-
-    newRow.appendChild(titleCell);
-    newRow.appendChild(authorCell);
-    newRow.appendChild(pagesCell);
-    newRow.appendChild(readCell);
-    newRow.appendChild(deleteCell);
-    deleteCell.appendChild(deleteBtn);
-
-    bookTable.appendChild(newRow);
-}
-
+// Display Form for adding books
 addButton.addEventListener('click', () => {
     addForm.style.display = 'block';
 })
 
+// Submit Book information to library, update table
 submitBook.addEventListener('click', (event) => {
     event.preventDefault();
 
@@ -95,12 +81,13 @@ submitBook.addEventListener('click', (event) => {
     newBook.read = document.querySelector('input[name="readYet"]:checked').value;
 
     addBookToLibrary(newBook);
-    addBookRow(newBook);
+    displayBooks();
 
     addForm.style.display = 'none';
     addForm.reset();
 })
 
+// Display initial library
 displayBooks();
 
 
